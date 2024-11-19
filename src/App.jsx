1bc8af31b33af4088a585c578e3c734e780db0bf
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-20 02:13:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,84 +40,18 @@ function App() {
   console.log(isLoading, user);
 
   return (
-    // <div className="flex flex-col overflow-hidden bg-white">
-    //   <Routes>
-    //     <Route
-    //       path="/"
-    //       element={
-    //         <CheckAuth
-    //           isAuthenticated={isAuthenticated}
-    //           user={user}
-    //         ></CheckAuth>
-    //       }
-    //     />
-    //     <Route
-    //       path="/auth"
-    //       element={
-    //         <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-    //           <AuthLayout />
-    //         </CheckAuth>
-    //       }
-    //     >
-    //       <Route path="login" element={<AuthLogin />} />
-    //       <Route path="register" element={<AuthRegister />} />
-    //     </Route>
-    //     <Route
-    //       path="/admin"
-    //       element={
-    //         <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-    //           <AdminLayout />
-    //         </CheckAuth>
-    //       }
-    //     >
-    //       <Route path="dashboard" element={<AdminDashboard />} />
-    //       <Route path="products" element={<AdminProducts />} />
-    //       <Route path="orders" element={<AdminOrders />} />
-    //       <Route path="features" element={<AdminFeatures />} />
-    //     </Route>
-    //     <Route
-    //       path="/shop"
-    //       element={
-    //         <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-    //           <ShoppingLayout />
-    //         </CheckAuth>
-    //       }
-    //     >
-    //       <Route path="home" element={<ShoppingHome />} />
-    //       <Route path="listing" element={<ShoppingListing />} />
-    //       <Route path="checkout" element={<ShoppingCheckout />} />
-    //       <Route path="account" element={<ShoppingAccount />} />
-    //       <Route path="paypal-return" element={<PaypalReturnPage />} />
-    //       <Route path="payment-success" element={<PaymentSuccessPage />} />
-    //       <Route path="search" element={<SearchProducts />} />
-    //     </Route>
-    //     <Route path="/unauth-page" element={<UnauthPage />} />
-    //     <Route path="*" element={<NotFound />} />
-    //   </Routes>
-    // </div>
-
+    
     <div className="flex flex-col overflow-hidden bg-white">
   <Routes>
-    {/* <Route path="/" element={<ShoppingLayout />} /> */}
-  
     <Route path="/" element={<ShoppingLayout />}>
-      <Route path="/" element={<ShoppingHome />} />
+      <Route index element={<ShoppingHome />} />
       <Route path="/shop/listing" element={<ShoppingListing />} />
       <Route path="/shop/checkout" element={<ShoppingCheckout />} />
       <Route path="/aboutus" element={<AboutPage />} />
       <Route path="/contactus" element={<ContactPage />} />
-      {/* <Route path="/shop/account" element={<ShoppingAccount />} /> */}
-      {/* <Route path="/shop/paypal-return" element={<PaypalReturnPage />} /> */}
-      {/* <Route path="/shop/payment-success" element={<PaymentSuccessPage />} /> */}
       <Route path="/shop/search" element={<SearchProducts />} />
     </Route>
-
-    {/* <Route path="/auth" element={<AuthLayout />}>
-      <Route path="login" element={<AuthLogin />} />
-      <Route path="register" element={<AuthRegister />} />
-    </Route> 
-    <Route path="/unauth-page" element={<UnauthPage />} />
-    <Route path="*" element={<NotFound />} /> */}
+    <Route path="*" element={<NotFound />} />
   </Routes>
 </div>
 
